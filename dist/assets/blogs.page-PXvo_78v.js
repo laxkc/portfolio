a1173bd9e309dfafd52415586e4ba831855d419e
Laxman KC <matrix4025@gmail.com>
2025-11-06 23:49:56 -0500

    Remove workflow and cleanup files

--- dist/assets/blogs.page-PXvo_78v.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-D08JS7uN.js";function a(){return e("div",{className:"mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16",children:e("p",{className:"text-2xl font-bold",children:"Welcome to Blogs Page"})})}export{a as default};
