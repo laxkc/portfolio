a75062eac2e9ad1ea2fd612750f90638630f7722
Laxman KC <matrix4025@gmail.com>
2025-11-06 22:37:02 -0500

    fix: button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import { type ComponentProps } from "preact"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -42,14 +42,22 @@ function Button({
   size,
   asChild = false,
   ...props
-}: React.ComponentProps<"button"> &
+}: ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
-  const Comp = asChild ? Slot : "button"
+  if (asChild) {
+    return (
+      <Slot
+        data-slot="button"
+        className={cn(buttonVariants({ variant, size, className }))}
+        {...(props as ComponentProps<typeof Slot>)}
+      />
+    )
+  }
 
   return (
-    <Comp
+    <button
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
